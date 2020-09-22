1b9d159ffad36469351cb05c49bd093c8e1d5389
IoTKETI <iot.keti@gmail.com>
2020-09-22 22:03:21 +0900

    Update msw_sparrow_air.js

--- msw_sparrow_air.js
+++ msw_sparrow_air.js
@@ -110,7 +110,7 @@ function runLib(obj_lib) {
             scripts_arr[0] = './' + config.directory_name + '/' + scripts_arr[0];
         }
 
-        var run_lib = spawn(scripts_arr[0], scripts_arr[1]);
+        var run_lib = spawn(scripts_arr[0], scripts_arr.slice(1));
 
         run_lib.stdout.on('data', function(data) {
             console.log('stdout: ' + data);
