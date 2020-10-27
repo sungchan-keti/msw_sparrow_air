d38f351c296842daf3046e452db7a47564893685
dnjstjr93 <dnjstjr93@gmail.com>
2020-10-27 11:29:34 +0900

    - edit run lib command

--- msw_sparrow_air.js
+++ msw_sparrow_air.js
@@ -111,7 +111,7 @@ function runLib(obj_lib) {
         }
 
         // var run_lib = spawn(scripts_arr[0], scripts_arr.slice(1));
-        var run_lib = spawn('python3', ['lib_sparrow_air', '/dev/ttyUSB4', '115200']);
+        var run_lib = spawn('python3', ['lib_sparrow_air.py', '/dev/ttyUSB4', '115200']);
 
         run_lib.stdout.on('data', function(data) {
             console.log('stdout: ' + data);
